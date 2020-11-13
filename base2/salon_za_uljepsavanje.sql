cbb8313811ebd8d8a2ea76dd00be7aa7670762e7
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-13 22:55:49 +0100

    Added comments and improvement suggestions

--- base2/salon_za_uljepsavanje.sql
+++ base2/salon_za_uljepsavanje.sql
@@ -7,7 +7,9 @@ create table djelatnica(
     sifra       int not null,
     ime         varchar(50),
     prezime     varchar(50),
-    oib         int not null,
+	-- why not use 'oib' as 'sifra' since its guaranteed to be unique?
+	-- oib should be char. what if oib starts with a 0?
+    oib         char(13) not null,
     iban        varchar(50)
 );
 
@@ -15,13 +17,18 @@ create table korisnik(
     sifra       int not null,
     ime         varchar(50),
     prezime     varchar(50),
-    oib         int not null
+	-- do we need a customer's oib?
+	-- if something is not null it means it's mandatory.
+    oib         char(13) not null
 );
 
 create table usluga(
     sifra   int not null,
     naziv   varchar(50),
-    cijena  decimal(12.5)
+	--12 digits, 2 digits for decimals
+    cijena  decimal(12, 2),
+	-- let's add a relationship between usluga and posjeta
+	posjeta int not null
 );
 
 create table posjeta(
@@ -29,3 +36,10 @@ create table posjeta(
     djelatnica int not null,
     korisnik int not null
 );
+
+alter table usluga
+	add foreign key (posjeta) references posjeta(sifra);
+-- add more foreign keys
+-- make a folder for this script
+-- add diagram for script
+-- push
