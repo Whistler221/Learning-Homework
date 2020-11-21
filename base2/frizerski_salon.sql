bb79a0edcc95e20f90d9023a0957c499e1c7ef67
Whistler221 <filip.klobucar1@hotmail.com>
2020-11-21 15:29:05 +0100

    new_test

--- base2/frizerski_salon.sql
+++ base2/frizerski_salon.sql
@@ -1,12 +1,11 @@
 drop database if exists frizerski_salon;
-create database frizerski_salon;
+create database frizerski_salon CHARACTER SET utf8mb4;
 
 use frizerski_salon;
 
 create table djelatnica(
     sifra       int not null primary key auto_increment,
     osoba       int not null,
-    usluga      int not null,
     iban        varchar(50)
 );
 
@@ -26,8 +25,7 @@ create table osoba(
 create table usluga(
     sifra   int not null primary key auto_increment,
     naziv   varchar(50) not null,
-    cijena  decimal(12,2),
-    posjeta int not null
+    cijena  decimal(12,2)
 );
 
 create table posjeta(
@@ -37,11 +35,38 @@ create table posjeta(
     usluga      int not null
 );
 
-alter table korisnik add foreign key (osoba) references osoba(sifra);
-
 alter table djelatnica add foreign key (osoba) references osoba(sifra);
+
+alter table korisnik add foreign key (osoba) references osoba(sifra);
 
 alter table posjeta add foreign key (djelatnica) references djelatnica(sifra);
 alter table posjeta add foreign key (korisnik) references korisnik(sifra);
+alter table posjeta add foreign key (usluga) references usluga(sifra);
 
-alter table usluga add foreign key (posjeta) references posjeta(sifra);
+insert into osoba (ime,prezime) values
+('Ivan','Abramovic'),
+('Snjezana','Misic'),
+('Marija','Simunovic'),
+('Dario','Horvat'),
+('Adam','Kuzmic');
+
+insert into djelatnica (osoba,iban) values
+(1,'HR3423400091658118587'),
+(2,'HR8724840081891152978'),
+(3,'HR5123400097326243441');
+
+insert into korosnik (osoba)
+select sifra from osoba where sifra>3;
+
+insert into usluga (naziv,cijena) values
+('Šišanje',34,99)
+('Prannje kose',20),
+('Bojanje',69,99);
+
+insert into posjeta (korisnik,djlatnica,usluga) values
+(1,2,1),
+(3,1,1),
+(2,3,2),
+(1,1,3),
+(4,1,2),
+(3,2,3);
