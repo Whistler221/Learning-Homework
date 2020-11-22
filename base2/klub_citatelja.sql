e73472ff687c562c3b7c4cd0113820a3d38404bc
Whistler221 <filip.klobucar1@hotmail.com>
2020-11-22 18:02:45 +0100

    added_values

--- base2/klub_citatelja.sql
+++ base2/klub_citatelja.sql
@@ -29,3 +29,18 @@ alter table knjiga add foreign key (vlasnik) references clan(sifra);
 
 alter table clan_knjiga add foreign key (clan) references clan(sifra);
 alter table clan_knjiga add foreign key (knjiga) references knjiga(sifra);
+
+insert into clan (ime, prezime, email)
+values ('ime1', 'prezime1', 'email@1'),
+		('ime2', 'prezime2', 'email@2'),
+		('ime3', 'prezime3', 'email@3');
+		
+insert into knjiga (naziv, vrsta, vlasnik)
+values ('naziv1', 'vrsta1', 1),
+		('naziv2','vrsta2', 2);
+		
+insert into clan_knjiga (clan, knjiga, stanje)
+values (1, 1, 1),
+		(2, 1 , 1),
+		(3, 2, 1),
+		(1, 2, 1);
