b5a79bab94f5c0550bf32c1bc9994c1b71281611
Whistler221 <filip.klobucar1@hotmail.com>
2020-11-22 17:01:33 +0100

    added_values

--- base2/doc_ordinacija2.sql
+++ base2/doc_ordinacija2.sql
@@ -14,6 +14,7 @@ create table medicinska_sestra(
 );
 
 create table asistiranje_med_sestre(
+	sifra int not null primary key auto_increment,
     medicinska_sestra       int not null,
     ljecenje                int not null
 );
@@ -47,3 +48,31 @@ create table osoba(
  alter table doktor add foreign key (osoba) references osoba (sifra);
  alter table medicinska_sestra add foreign key (osoba) references osoba (sifra);
  alter table pacijent add foreign key (osoba) references osoba (sifra);
+ 
+insert into osoba (ime, prezime, iban)
+values ('Zvonimir', 'Crnkovic', 'HR2625000091961547851'),
+		('Damir', 'Marijanovic', 'HR1123600008555235951'),
+		('Ivana', 'Dujic', 'HR5724020067515522821'),
+		('Sandra', 'Condric', 'HR8825000096955458942'),
+		('Miroslav', 'Jozic', 'HR8124840088285658963'),
+		('Igor', 'Levic', 'HR6924840082649342744');
+	
+insert into doktor (osoba)		
+values (1),
+		(2);
+	
+insert into medicinska_sestra (osoba)
+values (3),
+		(4);
+
+insert into pacijent (osoba)
+values (5),
+		(6);
+		
+insert into ljecenje (doktor, pacijent , datum)
+values (1, 2, '2020-08-16'),
+		(2, 1, '2020-06-12');
+		
+insert into asistiranje_med_sestre (medicinska_sestra, ljecenje)
+values (1,1),
+		(2,2);
