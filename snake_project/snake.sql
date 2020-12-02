063f9fb747d264086c1e14612be2ff0ebc0325fb
Whistler221 <filip.klobucar1@hotmail.com>
2020-12-02 02:00:45 +0100

    project

---
+++ snake_project/snake.sql
@@ -0,0 +1,54 @@
+﻿drop database if exists snake; 
+create database snake;
+use snake;
+
+
+-- game field creation and handling --
+create table gameField(
+	x_coord			int not null,
+	y_coord			int not null,
+
+	snakeID			int not null
+);
+		-- handeling of the game field --
+
+
+ -- make the snake entity --
+create table SNAKE(
+	snakeID			int not null primary key auto_increment,
+	playerID		int not null,
+
+	x_coord			int not null,
+	y_coord			int not null,
+	scoreID			int not null,
+	headBumpCheck	bit DEFAULT(0)
+
+);
+
+
+-- player information --
+create table player(
+	playerID		int not null primary key auto_increment,
+	playerName		varchar(50) not null
+);
+
+
+-- score board creation and handling
+create table scoreBoard(
+	scoreBoardID	int not null primary key auto_increment,
+	gameID			int not null,
+	playerID		int not null,
+	score			int not null
+);
+
+
+
+alter table SNAKE add foreign key (playerID) references player(playerID);
+
+alter table gameField add foreign key (snakeID) references SNAKE(snakeID);
+
+alter table scoreBoard add foreign key (playerID) references player(playerID);
+
+	
+
+
