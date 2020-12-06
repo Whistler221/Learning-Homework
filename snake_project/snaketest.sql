5cc47a67240a0b0a27bdde0f97e2919e4e5f360a
Whistler221 <filip.klobucar1@hotmail.com>
2020-12-06 19:10:08 +0100

    snake_update

---
+++ snake_project/snaketest.sql
@@ -0,0 +1,40 @@
+﻿drop database if exists snake; 
+create database snake;
+use snake;
+
+
+
+
+
+
+
+-- make the snake entity --
+create table SNAKE(
+	snakeID			int not null primary key auto_increment,
+	playerID		int not null,
+
+	x_coord			int not null,
+	y_coord			int not null,
+	scoreID			int not null,
+	headBumpCheck	bit DEFAULT(0)
+);
+
+
+-- player information --
+create table player(
+	playerID		int not null primary key auto_increment,
+	playerName		varchar(50) not null
+);
+
+
+create table team(
+	playerID		int not null,
+	snakeID			int not null
+
+);
+
+
+
+alter table team add foreign key (playerID) references player(playerID);
+
+alter table team add foreign key (snakeID) references SNAKE(snakeID);
