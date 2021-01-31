ac29837e9fcad02522d86a7564f0a2d8dd2e61c0
Whistler221 <filip.klobucar1@hotmail.com>
2021-01-31 18:23:42 +0100

    ExerciseV03

---
+++ java/ExercisesV03/src/repeating/Circle.java
@@ -0,0 +1,33 @@
+package repeating;
+
+public class Circle {
+	
+	// The radius of the circle
+	double radius = 1;
+	
+	// Construct a circle object
+	Circle() {
+		
+	}
+	
+	// Construct a circle object
+	Circle(double newRadius) {
+		radius = newRadius;
+	}
+	
+	// Return the area of this circle
+	double getArea() {
+		return radius * radius * Math.PI;
+	}
+	
+	// Return the perimeter of this circle
+	double getPerimeter() {
+		return 2 * radius * Math.PI;
+	}
+	
+	// Set a new radius for this corcle
+	void setRadius(double newRadius) {
+		radius = newRadius;
+	}
+	
+}
